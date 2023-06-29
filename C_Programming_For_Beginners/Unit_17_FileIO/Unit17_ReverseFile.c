26b762660daf095bc13dff3f6bee44d2af0ff06a
eyay-eng <aaron.hale.m@gmail.com>
2023-06-29 11:58:47 -0400

    Completed Unit 17 Challenges

---
+++ C_Programming_For_Beginners/Unit_17_FileIO/Unit17_ReverseFile.c
@@ -0,0 +1,55 @@
+/*
+*
+*   Writtn By: Aaron Hale
+*   Date: 06/29/23
+*
+*   Challenge to practice finding position in file
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+    //File to read from
+    FILE *pFile,*tempFile = NULL;
+    char *fName = "samplefile.txt";
+    pFile = fopen(fName,"r");
+
+    //temp file to write to
+    char *tempName = "TempFile.txt";
+    tempFile = fopen(tempName,"w");
+    //Check if file is open 
+
+    if(pFile==NULL || tempFile == NULL){
+        printf("Failed to open file!\n");
+        return(-1);
+    }
+
+    //Move to end of file
+    fseek(pFile,0, SEEK_END);
+    int pos = ftell(pFile);
+
+
+    printf("total size of file is %d bytes\n",pos);
+
+    int i = 0;
+    while(i < pos){
+        i++;
+        fseek(pFile,-i,SEEK_END);
+        fputc(fgetc(pFile),tempFile);
+    }
+
+    //Close files
+    fclose(pFile);
+    fclose(tempFile);
+    
+    //Swap temp file to original file
+    remove(fName);
+    rename(tempName,fName);
+
+
+
+    return(0);
+}
